9e5d6d89f32e95f3e29965ea39aefc4eac9c8b06
RMRAmadushanka <rmramadushanka@gmail.com>
2024-04-05 00:55:49 +0530

    indent

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,10 @@
 import { firebaseAdminApp } from "../firebase/index.js";
 
-const authCheck = async (req,res,next) => {
-    
-   
-       const decodedToken = await firebaseAdminApp.auth().verifyIdToken(req.headers.authtoken);
-       console.log(decodedToken);
-      
+const authCheck = async (req, res, next) => {
+  const decodedToken = await firebaseAdminApp
+    .auth()
+    .verifyIdToken(req.headers.authtoken);
+  console.log(decodedToken);
 };
 
 export { authCheck };
