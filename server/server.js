1e7105258a0055de3c079cd9889713c9f8946e8f
RMRAmadushanka <rmramadushanka@gmail.com>
2024-04-05 01:09:25 +0530

    test

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import cors from 'cors'
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
@@ -9,25 +9,21 @@ import authRoutes from "./routes/authRoutes.js";
 import bodyParser from "body-parser";
 
 //Create app
-const app = express()
+const app = express();
 
 //Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"))
-app.use(bodyParser.json({limit:"2mb"}))
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
 //DB
-connectDB()
-
+connectDB();
 
 //Routes Middleware
-app.use('/api',authRoutes)
+app.use("/api", authRoutes);
 //Port
-const port  = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-})
-
-
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
