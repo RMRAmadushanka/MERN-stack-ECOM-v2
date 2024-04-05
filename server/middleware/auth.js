b2cf142b193ba11f5f282354520b70312d44ac66
RMRAmadushanka <rmramadushanka@gmail.com>
2024-04-05 23:41:09 +0530

    Added user save to mongodb

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,17 @@
 import { firebaseAdminApp } from "../firebase/index.js";
 
 const authCheck = async (req, res, next) => {
-  const decodedToken = await firebaseAdminApp
-    .auth()
-    .verifyIdToken(req.headers.authtoken);
-  console.log(decodedToken);
+  try {
+    const decodedToken = await firebaseAdminApp
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      err: "Invalid or expired token",
+    });
+  }
 };
 
 export { authCheck };
